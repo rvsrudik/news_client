cb59c84d442081c9dc04763238f035ae108d7ae1
rvsrudik <rvsrudik@gmail.com>
2018-06-02 15:51:36 +0300

    added styles to login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,17 +1,37 @@
 <html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+
+</head>
 <body>
-<h1>Login page</h1>
-
-<div class="login-form">
-
-    <label for="mail">Email</label> <input type="text" id="login_mail"> <br>
 
 
-    <label for="login_pass">Password</label> <input type="password" id="login_pass"> <br>
+<div class="container">
+    <div class="row h-100 d-flex justify-content-center align-items-center">
 
-    <div class="sign-in-btn btn">Sign In</div>
-    <div class="sign-up-btn btn">Sign Up</div>
+        <div class="login-block" >
+            <h1>News client</h1>
 
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="btn-block d-flex justify-content-between">
+                    <button type="submit" class="btn btn-success">Sign In</button><button type="submit" class="btn btn-primary">Sign Up</button>
+                </div>
+            </form>
+        </div>
+
+
+    </div>
 </div>
+
 </body>
 </html>
