004abb69b22f8e08b602e3da51e2d2badaddf55b
rvsrudik <rvsrudik@gmail.com>
2018-06-02 15:00:55 +0300

    added login view

---
+++ resources/views/login.blade.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<h1>Login page</h1>
+
+<div class="login-form">
+
+    <label for="mail">Email</label> <input type="text" id="login_mail"> <br>
+
+
+    <label for="login_pass">Password</label> <input type="password" id="login_pass"> <br>
+
+    <div class="sign-in-btn btn">Sign In</div>
+    <div class="sign-up-btn btn">Sign Up</div>
+
+</div>
+</body>
+</html>
